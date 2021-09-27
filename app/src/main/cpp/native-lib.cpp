19f8b3be8e6fa91cab22a5b7dc5265a3a96a0116
sdf <dianfengsun@126.com>
2021-09-27 17:02:15 +0800

    print opencv version.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,17 @@
 #include <jni.h>
 #include <string>
+#include "opencv2/opencv.hpp"
+
+using namespace std;
+using namespace cv;
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_opencv_stitch_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+    std::string hello = "Hello from opencv ";
+    char strBuffer[128];
+    sprintf(strBuffer, "Hello from opencv %s\n", CV_VERSION);
+//    hello += string(cv::CV_VERSION_MAJOR) ;
+    return env->NewStringUTF(strBuffer);
 }
